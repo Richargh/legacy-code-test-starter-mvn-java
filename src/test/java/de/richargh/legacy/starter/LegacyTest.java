09f6f4311c1e775868b6972fe266e757b44a47c8
Richard Gross <richard.gross@maibornwolff.de>
2022-12-17 21:50:01 +0100

    e: adding editorconfig and approval

--- src/test/java/de/richargh/legacy/starter/LegacyTest.java
+++ src/test/java/de/richargh/legacy/starter/LegacyTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-import java.util.Random;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
